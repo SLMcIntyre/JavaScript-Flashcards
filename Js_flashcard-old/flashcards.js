f98cb7e719c79b65e6a6514615de54b038ad601c
Shelley McIntyre <73217296+SLMcIntyre@users.noreply.github.com>
2021-01-06 09:02:20 -0500

    JS Flashcards

---
+++ Js_flashcard-old/flashcards.js
@@ -0,0 +1,16 @@
+// $(".flippable").click(function(){
+//     $(this).toggleClass("flipme");
+//   });
+
+// let js_Json = 
+// let obj = JSON.parse(text);
+// function ()=>{
+//     for (let i = 0; i<=obj.length-1; i++);
+    
+// }
+
+// document.getElementById("app").innerHTML = `<h1> JavascriptBasics`
+
+function preload() {
+    flashcards  = loadJSON("jsFlashcards.json");
+}
